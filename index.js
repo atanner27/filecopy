96ff0a35a585af8ad1bf97792fe80716f8e04fd5
Andrew Tanner <andrew.tanner@instacart.com>
2018-12-08 20:53:09 -0500

    Added shelljs
    Added shelljs

--- index.js
+++ index.js
@@ -4,8 +4,16 @@
  * @param {!express:Request} req HTTP request context.
  * @param {!express:Response} res HTTP response context.
  */
+var shell = require('shelljs');
+
 exports.helloWorld = (req, res) => {
+
   let message = req.query.message || req.body.message || 'Hello World!';
+
+  shell.ls('*.js').forEach(function (file) {
+    console.log(file);
+  });
+
   res.status(200).send(message);
 
 
